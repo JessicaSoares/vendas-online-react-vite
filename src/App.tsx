4948a4a63b699e8477e7a63964e7eeca04664832
jessica <jessicassoarez@gmail.com>
2023-11-17 01:28:52 -0300

    feat: Background

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,6 @@
-import { useState } from 'react'
-
-import { Button } from 'antd'
-import styled from 'styled-components'
-
-import reactLogo from './assets/react.svg'
-
-import viteLogo from '/vite.svg'
-
-import './App.css'
+import LoginScreen from './modules/login'
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <StyledLink>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </StyledLink>
-      <h1>Vite + React</h1>
-      <StyledLink className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is
-          {count}
-        </Button>
-        <p>Edit src/App.tsx and save to test HMR</p>
-      </StyledLink>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <LoginScreen />
 }
-const StyledLink = styled.div`
-  color: blue;
-  font-weight: bold;
-`
 
 export default App
