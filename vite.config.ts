3ca42986ace91fbf7301cf6ce342c3893e0b755c
jessica <jessicassoarez@gmail.com>
2023-11-16 22:29:58 -0300

    feat: lint run

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
@@ -19,4 +19,4 @@ export default defineConfig({
       '@/redux': path.resolve(__dirname, 'src/redux'),
     },
   },
-});
+})
